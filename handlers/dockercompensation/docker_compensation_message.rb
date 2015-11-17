5442d6a938b5f56ee93428d255b564f478eac4ff
Johannes M. Schleicher <johannes.schleicher@gmail.com>
2015-11-17 22:56:47 +0100

    Linked Docker Compensation Handler to MS architecture via MQ

--- handlers/dockercompensation/docker_compensation_message.rb
+++ handlers/dockercompensation/docker_compensation_message.rb
@@ -1,22 +1,22 @@
 require './lib/data_exchange.rb'
-require './docker_analyser.rb'
+require './docker_compensation.rb'
 
-class DockerAnalyserMessage
+class DockerCompensationMessage
   
     
   def start
     # Set the topic that u want to listen to
-    dataexchange = DataExchange.new('analysers')
+    dataexchange = DataExchange.new('compensation')
     dataexchange.wait_to_receive("artifact.uri",self)
   
   end
 
   def process(message)
-    analyser = DockerAnalyser.new
+    compensation = DockerCompensation.new
     message = message.split(",")
-    analyser.analyse(message[0],message[1])
+    # analyser.analyse(message[0],message[1])
   end
   
 end  
 
-DockerAnalyserMessage.new.start
+DockerCompensationMessage.new.start
