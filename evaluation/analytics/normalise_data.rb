9d450d8a84ea18a0f9f2a05a1bbbe1eefb76f382
Johannes M. Schleicher <johannes.schleicher@gmail.com>
2015-11-24 10:46:10 +0100

    Refactoring
    Former-commit-id: d0c0eae39d15bbaef75cf207e0bc3d9cf950e74e

--- evaluation/analytics/normalise_data.rb
+++ evaluation/analytics/normalise_data.rb
@@ -3,3 +3,16 @@ require 'json'
 require 'mongo'
 require 'gnuplot'
 require 'rest-client'
+
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'analytics')
+
+
+### QUERIES
+#
+# { vulnerabilities: {$exists: true, $gt: {$size: 0}} }
+#
+# # Official
+# { image_name: {$regex: '^[^/]*$'} }
+#
+# # Not official
+# { image_name: {$regex: '.+/.+'} }
